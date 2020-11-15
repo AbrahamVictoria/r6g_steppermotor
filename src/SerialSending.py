60d2fcc13b4a66a52741639a52c556eb9d535c81
ATevera <atevera.rz@gmail.com>
2020-11-15 12:51:47 -0600

    Control EfectorFinal

--- src/SerialSending.py
+++ src/SerialSending.py
@@ -1,28 +1,16 @@
 #! /usr/bin/env python3
 import rospy
 import math
-from sensor_msgs.msg import JointState
+from r6g_steppermotor.msg import CurrentPose
 
 import serial, time
 
-
-
 def SendData(name, target):
 	"""Envío de datos por el puerto serial con estructura JSON"""
-	if arduino.readable():
-		arduino.write(target.encode())
-	#print("{}: {}".format(name, target))
-	"""
-	while True:
-		confirm = str(arduino.readline())
-		#print(confirm)
-		if 'OK' in confirm: 
-			break
-		time.sleep(1)
-	print("OK!")
-	"""
+	if esp32.readable():
+		esp32.write(target.encode())
 
-def callback(data):
+def ToJSONFile(robot):
 	"""Construcción del mensaje en formato JSON"""
 	
 	target = "{"
@@ -31,16 +19,15 @@ def callback(data):
 
 	comparative = True
 
-	for grado in data.position:
+	for grado in robot.position:
 		grado_Degrees = round(math.degrees(grado),4)
 		comparative = grado_Degrees == valuesDoF[i] and comparative 
 		valuesDoF[i] = grado_Degrees
 		target += "\"J{}\": {},".format(i+1,grado_Degrees)
 		estado += "J{}: {} ".format(i+1,grado_Degrees)
 		i += 1
-	#target += "\"J{}\": {}".format(6,0)
-	EndEffector = int(input("Estado del efector final: "))
-	target += "\"EF\": {}".format(EndEffector)
+	target += "\"EF\": {}".format(robot.endEffector)
+	estado += "EF: {}".format(robot.endEffector)
 	target += "}"
 	PosName = "Position"
 	if not comparative:
@@ -50,15 +37,15 @@ def callback(data):
 def SerialSending():
 	"""Inicialización del nodo suscriptor"""
 	rospy.init_node('SerialSending', anonymous=True)
-	rospy.Subscriber("joint_states", JointState, callback)
+	rospy.Subscriber("CurrentPose", CurrentPose, ToJSONFile)
 	print('Nodo creado con Éxito')
 	rospy.spin()
 
 #Inicialización del puerto serie
 port = '/dev/ttyUSB0'
 #port = 'COM4'
-arduino = serial.Serial(port, 115200)
-arduino.setDTR = False
+esp32 = serial.Serial(port, 115200)
+esp32.setDTR = False
 print('Puerto serial iniciado en {}'.format(port))
 valuesDoF = [0, -71, 75, 0, 0, 0]
 
@@ -66,5 +53,5 @@ if __name__ == '__main__':
 	try:
 		SerialSending()
 	except rospy.ROSInterruptException:
-		arduino.close()
+		esp32.close()
 
--- src/StepControl.py
+++ src/StepControl.py
@@ -1,23 +1,24 @@
 #! /usr/bin/env python3
 
 import rospy
-from sensor_msgs.msg import JointState
+from r6g_steppermotor.msg import CurrentPose
 
 JointValues = [0]*7
 
 def StepControl():
 	rospy.init_node('StepControl',anonymous = True)
-	pub = rospy.Publisher('joint_states',JointState,queue_size=100)
+	pub = rospy.Publisher('CurrentPose', CurrentPose, queue_size=100)
 	print('Nodo creado con Éxito')
 	rate = rospy.Rate(10) #Frecuencia de publicaciÃ³n -> 10Hz
-	grados = JointState()
-	grados.position = [0]*6
-	grados.name = ['Joint1', 'Joint2', 'Joint3', 'Joint4', 'Joint5', 'Joint6']
+	r6g = CurrentPose()
+	r6g.position = [0]*6
+	r6g.name = ['Joint1', 'Joint2', 'Joint3', 'Joint4', 'Joint5', 'Joint6']
 	i = 1
 	while not rospy.is_shutdown():
 		for i in range(0,6):
-			grados.position[i] = float(input("J{}: ".format(i+1)))
-		pub.publish(grados)
+			r6g.position[i] = float(input("J{}: ".format(i+1)))
+		r6g.endEffector = int(input("Estado del efector final: "))
+		pub.publish(r6g)
 		rate.sleep()
 
 if __name__ == '__main__':
